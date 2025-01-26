17aa94af7ee1dc4eb1762f1c773edd8189ed08f0
aniekan <77311836+aniekanBane@users.noreply.github.com>
2025-01-26 15:29:21 +0100

    modify - contd. application layer setup
    - added riok.Mapperly for mapping
    - removed mapster dpendency
    - updated pre processors
    - seo entry and validator

--- src/Core/Application/DependencyInjection.cs
+++ src/Core/Application/DependencyInjection.cs
@@ -1,8 +1,6 @@
-﻿using System.Reflection;
-using eCommerceWeb.Application.Pipeline.Behaviors;
+﻿using eCommerceWeb.Application.Pipeline.Behaviors;
 using eCommerceWeb.Application.Pipeline.PreProcessors;
 using FluentValidation;
-using Mapster;
 using MediatR.Pipeline;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,22 +8,14 @@ namespace eCommerceWeb.Application;
 
 public static class DependencyInjection
 {
-    public static readonly Assembly AssemblyReference = typeof(DependencyInjection).Assembly;
-
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // Mapster
-        var typeAdapterConfig = TypeAdapterConfig.GlobalSettings;
-        typeAdapterConfig.EnableImmutableMapping();
-        typeAdapterConfig.Scan(AssemblyReference);
-        services.AddSingleton(typeAdapterConfig);
-
-        services.AddValidatorsFromAssembly(AssemblyReference, includeInternalTypes: true);
+        services.AddValidatorsFromAssembly(typeof(IApplicationMarker).Assembly, includeInternalTypes: true);
 
         // MediatR
         services.AddMediatR(cfg =>
         {
-            cfg.RegisterServicesFromAssembly(AssemblyReference);
+            cfg.RegisterServicesFromAssembly(typeof(IApplicationMarker).Assembly);
             cfg.AddRequestPreProcessor(typeof(IRequestPreProcessor<>), typeof(ValidationPreProcessor<>));
             cfg.AddOpenBehavior(typeof(PerformanceBehavior<,>));
             cfg.AddOpenBehavior(typeof(RequestExceptionProcessorBehavior<,>));
